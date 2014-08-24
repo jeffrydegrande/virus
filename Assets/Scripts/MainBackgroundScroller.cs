81e34c30b1639ee3ab3468ee8fece55488385da2
Jeffry Degrande <jeffry@jeffrydegrande.com>
2014-08-24 20:54:50 -0300

    fixing main background scrolling

---
+++ Assets/Scripts/MainBackgroundScroller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainBackgroundScroller : MonoBehaviour {
+
+	public float scrollSpeed = 12f;
+	public float resetx = -100f;
+	private Vector3 startPosition;
+	
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log ( renderer.bounds.size );
+
+		transform.Translate(Vector3.left*scrollSpeed*Time.deltaTime);
+		
+		if (transform.position.x < resetx) { // moved off screen
+			transform.position = startPosition;
+		}
+	}
+}
