31fa9db7bf58659e80856098f21701fb98ec4fee
Jeffry Degrande <jeffry@jeffrydegrande.com>
2014-08-24 18:03:24 -0300

    clean up bullets when moving off screen

---
+++ Assets/BulletHit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletHit : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {	
+		if (coll.gameObject.tag == "BulletCollector") {
+			Destroy (this.gameObject);
+
+		}
+	}
+}
