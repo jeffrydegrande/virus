7cf49ae12df8e8cc9455d0840eaf4f27a6b0c69e
Jeffry Degrande <jeffry@jeffrydegrande.com>
2014-08-25 18:21:50 -0300

    spawning viruses

---
+++ Assets/SpawnViruses.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnViruses : MonoBehaviour {
+
+	public GameObject virus;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Spawn());
+	}
+
+	IEnumerator Spawn() {
+		while (true) {
+			yield return new WaitForSeconds(Random.Range (2f, 4f));
+
+			Vector3 p = transform.position;
+			p.y = Random.Range (-15f, 19f);
+
+			GameObject clone = (GameObject)Instantiate(virus, p, transform.rotation);
+			clone.rigidbody2D.velocity = transform.right * - Random.Range (40f, 60f);
+
+
+		}
+	}
+}
