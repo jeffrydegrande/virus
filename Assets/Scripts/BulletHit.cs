3649cbec6214cbe5eb55ed482d4662a775d73048
Jeffry Degrande <jeffry@jeffrydegrande.com>
2014-08-25 19:09:20 -0300

    fix scoring and set hitpoints to 2

--- Assets/Scripts/BulletHit.cs
+++ Assets/Scripts/BulletHit.cs
@@ -2,17 +2,9 @@
 using System.Collections;
 
 public class BulletHit : MonoBehaviour {
-
-	GameObject score;
-
-	void Start() {
-		score = GameObject.Find ("Score");
-	}
-
 	void OnCollisionEnter2D(Collision2D coll) {	
 		if (coll.gameObject.tag == "BulletCollector") {
 			Destroy (this.gameObject);
-			score.SendMessage("Increase", 1);
 		}
 	}
 	
--- Assets/TakeDamage.cs
+++ Assets/TakeDamage.cs
@@ -7,16 +7,25 @@ public class TakeDamage : MonoBehaviour {
 	public AudioClip monsterHit;
 	public AudioClip monsterExplodes;
 	public GameObject game;
+	GameObject score;
+
+
+	public Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		game = GameObject.Find("Game");
+		score = GameObject.Find ("Score");
 	}
 
 
 	public void ApplyDamage(int value) {
 		hitpoints -= value;
 		game.audio.PlayOneShot (monsterHit);
+
+		if (anim) {
+			anim.SetBool ("Hit", true);
+		}
 
 		if (hitpoints <= 0) {
 			Die();
@@ -26,6 +35,9 @@ public class TakeDamage : MonoBehaviour {
 
 	void Die()
 	{
+		score.SendMessage("Increase", 1);
+
+		anim.SetBool("Dead", true);
 		Destroy(gameObject);
 		game.audio.PlayOneShot(monsterExplodes);
 	}
