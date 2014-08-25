627f951bb9bb58256f41d519b5505fc458b8e407
Jeffry Degrande <jeffry@jeffrydegrande.com>
2014-08-25 14:41:13 -0300

    intro scene & scoring

---
+++ Assets/IncreaseScore.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseScore : MonoBehaviour {
+
+	int theScore = 0;
+
+
+	void Increase( int score ) {
+		theScore += score;
+
+		this.guiText.text = "SCORE: " + theScore;
+	}
+}
