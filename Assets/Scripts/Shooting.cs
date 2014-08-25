69edfc029b98aff9fc859217b3b3c464d4ba3d7d
Jeffry Degrande <jeffry@jeffrydegrande.com>
2014-08-25 14:57:13 -0300

    put in firing audio

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,11 +6,17 @@ public class Shooting : MonoBehaviour {
 	// private GameObject player;
 	public Rigidbody2D projectile;
 	public float projectileSpeed = 120f;
-	
+
+	public AudioClip laserSound;
+
 	void Update () {
 		if (Input.GetButtonDown("Fire")) {
 			Rigidbody2D bulletClone = (Rigidbody2D)Instantiate (projectile, transform.position, transform.rotation);
 			bulletClone.velocity = transform.right * projectileSpeed;
+
+			if (laserSound) {
+				audio.PlayOneShot(laserSound);
+			}
 		}
 	}
 }
